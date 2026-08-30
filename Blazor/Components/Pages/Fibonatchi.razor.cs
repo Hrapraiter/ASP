00eede82c983970ea883d56e742aaa64e82eea89
Hrapraiter <223995225+Hrapraiter@users.noreply.github.com>
2026-08-30 09:42:00 +0700

    Power and Fibonatchi DONE

---
+++ Blazor/Components/Pages/Fibonatchi.razor.cs
@@ -0,0 +1,38 @@
+﻿namespace Blazor.Components.Pages
+{
+    using Microsoft.AspNetCore.Html;
+    using System.Numerics;
+    public partial class Fibonatchi
+    {
+        int n;
+        List<BigInteger> cashNumbers = new List<BigInteger>() { 0 , 1 , 1 };
+        string output = "";
+
+        int getIntLenght(double value) 
+        {
+            int count = 0;
+            while(value > 0) 
+            {
+                value = Math.Floor(value / 10);
+                count++;
+            }
+            return count-1;
+        }
+        void Calculate()
+        {
+            output = "";
+
+            if(n > cashNumbers.Count)
+                 for(int i = cashNumbers.Count-1; i < n; i++) 
+                     cashNumbers.Add(cashNumbers[i - 1] + cashNumbers[i]);
+
+            //int numbersCount = Math.Min(n, cashNumbers.Count);// страховка
+            int spaceCount = getIntLenght(n);
+            for(int i = 0; i < n; i++)
+            {
+                output += $"{new string(' ' , spaceCount - getIntLenght(i+1))}{i+1}. {cashNumbers[i]}\n";
+            }
+            
+        }
+    }
+}
---
+++ Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlTypes;
+using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        int number = 0;
+        int pow = 0;
+        //BigInteger positiveResult = 0;
+        Double negativeResult = 0;
+        string output = "";
+        void Calculate()
+        {
+            output = "";
+            bool isPositive = pow >= 0 ? true : false;
+            if (!isPositive)
+            {
+                negativeResult = number;
+                for (int i = 0; i >= pow; i--) 
+                {
+                    negativeResult /= number;
+                }
+                output =    negativeResult.ToString("F99").TrimEnd('0').TrimEnd('.');
+            }
+            else 
+            {
+                output = BigInteger.Pow(number, pow).ToString();
+            }
+        }
+    }
+}
