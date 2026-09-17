974f929a5c53e438cce96f56b70cc4544b5778ea
Hrapraiter <223995225+Hrapraiter@users.noreply.github.com>
2026-09-18 00:54:15 +0700

    Lesson 24 - Directions DONE

---
+++ Academy/AcademyContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AcademyContext(DbContextOptions<AcademyContext> options) : DbContext(options)
+{
+    public DbSet<Academy.Models.Direction> Directions { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
