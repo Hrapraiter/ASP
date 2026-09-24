d33540d66628e2576a03460a88eec7de6009472f
Hrapraiter <223995225+Hrapraiter@users.noreply.github.com>
2026-09-24 14:53:08 +0700

    Lesson 27 - LeraningDays Extract DONE

---
+++ Academy/Components/Pages/GroupPages/Week.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+
+namespace Academy.Components.Pages.GroupPages
+{
+    public class Week
+    {
+        public static readonly string[] DAYNAMES =
+            { "Пн" ,"Вт" ,"Ср" ,"Чт" ,"Пт" ,"Сб" ,"Вс" };
+        
+        int days;
+        public Week(int days) 
+        {
+            this.days = days;
+        }
+        public override string ToString()
+        {
+            List<string> list = new List<string>();
+            for(byte i = 0; i < 7; i++) 
+            {
+                if(((days >> i) & 1) != 0)
+                    list.Add(DAYNAMES[i]);
+            }
+            return string.Join(", " ,list);
+        }
+    }
+}
