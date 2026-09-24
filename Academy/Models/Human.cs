ac14dfd65982bea968d238807e972cf286a26f4a
Hrapraiter <223995225+Hrapraiter@users.noreply.github.com>
2026-09-24 23:17:32 +0700

    Lesson 29 - Student model DONE , TODO: Scaffold Student.

---
+++ Academy/Models/Human.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+    public class Human
+    {
+        [Required]
+        [StringLength(maximumLength:50 , MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Фамилия содержит недопустимые символы")]
+        public string last_name { get; set; }
+        [Required]
+        [StringLength(maximumLength:50 , MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Имя содержит недопустимые символы")]
+        public string first_name { get; set; }
+        [StringLength(maximumLength:50 , MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Отчество содержит недопустимые символы")]
+        public string? middle_name { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateOnly), "1950-01-01", "2020-01-01")]
+        DateOnly birth_date { get; set; }
+        public string? email { get; set; }
+        public string? phone { get; set; }
+        public byte[]? photo { get; set; }
+    }
+}
